3f884168e5dc1e9b55aff70a430054cd4a8dfe11
Andreas Gale Dwi Jaya <andreasgaledwijaya@gmail.com>
2024-09-12 23:47:34 +0700

    Pertemuan 3 - Implementasi Eloquent ORM

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+        // Tambah data user dengan Eloquent Model
+        // $data = [
+        //     'username' => 'Customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 3
+        // ];
+        // UserModel::insert($data); // Tambahkan data ke tabel m_user
+
+        // Coba akses model UserModel
+        $user = UserModel::all(); // Ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'Customer-1')->update($data);
+    }
+}
