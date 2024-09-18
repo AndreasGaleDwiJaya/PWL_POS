a7eec55c6b81daa8e002618f034f9d6575ed8d3b
Andreas Gale Dwi Jaya <andreasgaledwijaya@gmail.com>
2024-09-18 17:27:49 +0700

    Pertemuan 4 - Praktikum 2.4 Retreiving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -59,7 +59,20 @@ class UserController extends Controller
         //Jobsheet 4 - Praktikum 2.3 
         // $user = UserModel::where('level_id', 2)->count();
         // dd($user);
-        $user = UserModel::where('level_id', 2)->count(); 
+        // $user = UserModel::where('level_id', 2)->count(); 
+        // return view('user',['data' => $user]);
+
+        //Jobsheet 4 - Praktikum 2.4 
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user -> save();
+
         return view('user',['data' => $user]);
     }
 }
