9992d49df0eac3003c6c5f3080837cbc7b68eb30
Andreas Gale Dwi Jaya <andreasgaledwijaya@gmail.com>
2024-10-09 14:27:04 +0700

    Pertemuan 7 - Praktikum 1 dan Tugas 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,35 +2,40 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
-    // Nama tabel
-    protected $table = 'm_user'; 
-    
-    // Kolom primary key
-    protected $primaryKey = 'user_id'; 
-    
-    // Jika kunci utama tidak auto-incrementing, tambahkan:
-    public $incrementing = false;
-    
-    // Format primary key (jika bukan integer, misalnya string)
-    protected $keyType = 'string'; // Ganti sesuai tipe data user_id (misalnya integer)
-    
-    // Aktifkan timestamps jika menggunakan created_at dan updated_at
-    public $timestamps = true;
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['level_id', 'username', 'name', 'password', 'created_at', 'updated_at'];
 
-    //Jobsheet 4-Prartikum 1
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $hidden = ['password'];
 
-        public function level(): BelongsTo
+    protected $casts = ['password' => 'hashed'];
+
+    public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode == $role;
+    }
+
+    public function getRole()
+    {
+        return $this->level->level_kode;
+    }
 }
